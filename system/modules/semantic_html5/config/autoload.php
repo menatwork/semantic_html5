214cd4d84589fafa7ed3e9305861d05aa952fb7f
Patrick Kahl <kahl.patrick@googlemail.com>
2013-07-10 11:22:56 +0200

    Add autoload for contao 3

---
+++ system/modules/semantic_html5/config/autoload.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2013
+ * @package    semantic_html5
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'SemanticHTML5'       => 'system/modules/semantic_html5/SemanticHTML5.php',
+	'SemanticHTML5Helper' => 'system/modules/semantic_html5/SemanticHTML5Helper.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'ce_semantic_html5' => 'system/modules/semantic_html5/templates',
+));
